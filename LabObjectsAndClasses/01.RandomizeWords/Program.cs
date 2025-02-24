13a44de07404cf227e1109aa1248d6abc3bbb11d
Kristiayn <kristodd2701@gmail.com>
2025-02-24 16:49:23 +0200

    ObjectsAndClasses

---
+++ LabObjectsAndClasses/01.RandomizeWords/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _01.RandomizeWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split();
+
+            Random random = new();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                int randomIndex = random.Next(0, words.Length);
+
+                SwapWords(i, randomIndex, words);
+            }
+
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+
+        static void SwapWords(int firstIndex, int secondIndex, string[] words)
+        {
+            string temp = words[firstIndex];
+            words[firstIndex] = words[secondIndex];
+            words[secondIndex] = temp;
+        }
+    }
+}
